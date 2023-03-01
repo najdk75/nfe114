594c1530960b0fa76cfdf9ee7f0ed5c658e0f075
najdk75 <107084044+najdk75@users.noreply.github.com>
2023-03-01 12:32:48 +0100

    test

--- recherche.php
+++ recherche.php
@@ -9,7 +9,7 @@ $ANNEE = $_GET['annee'];
 
 
 //1° - Connexion à la BDD
-$base = new PDO('mysql:host=localhost; dbname=id20205717_database_movies', 'id20205717_najd', 'D9E2OU{VK>44rv^Z');
+$base = new PDO('mysql:host=localhost; dbname=id20205717_movies', 'id20205717_najd', 'nf(@QdchWm2Qb/()');
 $base->exec("SET CHARACTER SET utf8");
 //2° - Préparation de requette et execution
 $retour = $base->query('SELECT * FROM movies WHERE annee='.$ANNEE);
