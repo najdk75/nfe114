412cbec74a23f69f0498cee50fcfeae838270773
najdk75 <107084044+najdk75@users.noreply.github.com>
2023-03-01 11:06:33 +0100

    liste films

---
+++ liste.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+<h1>Liste des films</h1>
+
+
+<?php
+//Lister le contenu de la table movies
+
+//1° - Connexion à la BDD
+$base = new PDO('mysql:host=localhost; dbname=id20215400_cnamgreta', 'id20215400_boris', '=nq^^=vKz?FRiz7-');
+$base->exec("SET CHARACTER SET utf8");
+
+//2° - Préparation de requette et execution
+$retour = $base->query('SELECT * FROM movies;');
+
+//3° - Lecture du resultat de la requette
+while ($data = $retour->fetch()){
+echo $data['id']." ".$data['titre']." ".$data['genre']." ".$data['annee']."</br>";
+}
+
+?>
+
+</body>
+</html>
