e4501485afd7e9b2b86828d4a40cca5da4164208
najdk75 <107084044+najdk75@users.noreply.github.com>
2023-03-01 11:55:01 +0100

    test formulaire recherche par année

--- recherche.php
+++ recherche.php
@@ -1,24 +1,29 @@
 <html>
 <body>
-<h1>Insertion du film</h1>
+<h1>Recherche de films</h1>
 <?php
 
-$TITRE= $_GET['titre'];
-$GENRE = $_GET['genre'];
+
 $ANNEE = $_GET['annee'];
 
-echo $TITRE." ".$GENRE." ".$ANNEE; 
+
 
 //1° - Connexion à la BDD
 $base = new PDO('mysql:host=localhost; dbname=id20205717_database_movies', 'id20205717_najd', 'QEyHS[))ia1DAg1X');
 $base->exec("SET CHARACTER SET utf8");
-$sql = 'INSERT INTO movies VALUES(NULL,"'.$TITRE.'","'.$GENRE.'",'.$ANNEE.')';
+$sql = "SELECT title FROM movies WHERE annee='" . $ANNEE . "'";
 //2° - Préparation de requette et execution
 $retour = $base->query($sql);
 
+$resultats = $base->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+// Affichage des résultats
+foreach ($resultats as $resultat) {
+    echo $resultat['title'] . "<br>";
+}
+
 
 ?>
-
 
 </body>
 </html>
