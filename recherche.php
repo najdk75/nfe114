700ae688f1bb3627089346fc7cfa033e01dfd2b3
najdk75 <107084044+najdk75@users.noreply.github.com>
2023-03-01 12:00:05 +0100

    encore test

--- recherche.php
+++ recherche.php
@@ -11,7 +11,6 @@ $ANNEE = $_GET['annee'];
 //1° - Connexion à la BDD
 $base = new PDO('mysql:host=localhost; dbname=id20205717_database_movies', 'id20205717_najd', 'QEyHS[))ia1DAg1X');
 $base->exec("SET CHARACTER SET utf8");
-$sql = "SELECT title FROM movies WHERE annee='" . $ANNEE . "'";
 //2° - Préparation de requette et execution
 $retour = $base->query('SELECT * FROM movies WHERE annee='.$ANNEE.';');
 
