212f18ec69d7a49abb21067aeb68a69e330af69b
najdk75 <107084044+najdk75@users.noreply.github.com>
2023-03-02 11:09:05 +0100

    musée test

--- POI.php
+++ POI.php
@@ -3,6 +3,8 @@
 //C'est le POI de l'utilisateur
 echo "lat\tlon\ttitle\tdescription\ticon\ticonSize\ticonOffset\n";
 echo "48.858205\t2.294359\tMoi\tMa Position\tOl_icon_blue_example.png\t24,24\t0,-24\n";
+echo "2.35576300\t48.84205300\tMuséum national d'Histoire naturelle\tmuseum.png\t24,24\t0,-24\n;
+
 
 //1° - Connexion à la BDD
 $base = new PDO('mysql:host=localhost; dbname=id20205717_open_street_map', 'id20205717_najd2', 'mhV@4(qdgb()k+5G');
