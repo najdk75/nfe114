ec60cb72c4fb808d2933fe9770fcaa5051c3f16d
najdk75 <107084044+najdk75@users.noreply.github.com>
2023-03-02 11:35:56 +0100

    test marseille

--- POI.php
+++ POI.php
@@ -2,8 +2,7 @@
 
 //C'est le POI de l'utilisateurr
 echo "lat\tlon\ttitle\tdescription\ticon\ticonSize\ticonOffset\n";
-echo "48.858205\t2.294359\tMoi\tMa Position\tOl_icon_blue_example.png\t24,24\t0,-24\n";
-echo "2.35576300\t48.84205300\Museum d'Histoire Naturelle de Paris\tMa Position\tOl_icon_blue_example.png\t24,24\t0,-24\n";
+echo "43.296482\t5.36978\tMoi\tMa Position\tOl_icon_blue_example.png\t24,24\t0,-24\n";
 
 
 //1° - Connexion à la BDD.
