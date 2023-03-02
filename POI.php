39a6c65391db860f59c96735d1d53ee885eae39a
najdk75 <107084044+najdk75@users.noreply.github.com>
2023-03-02 12:52:12 +0100

    changement off_set

--- POI.php
+++ POI.php
@@ -5,7 +5,7 @@ $LONGITUDE = $_GET['longitude'];
 $LATITUDE = $_GET['latitude'];
 //C'est le POI de l'utilisateur
 echo "lat\tlon\ttitle\tdescription\ticon\ticonSize\ticonOffset\n";
-echo "$LATITUDE\t$LONGITUDE\tMoi\tMa Position\tperson.png\t24,24\t0,-24\n";
+echo "$LATITUDE\t$LONGITUDE\tMoi\tMa Position\tperson.png\t24,24\t0,0\n";
 
 
 //1° - Connexion à la BDD...
