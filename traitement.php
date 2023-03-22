1ba44f9485db8ced6ef24439230686d95d30e056
najdk75 <107084044+najdk75@users.noreply.github.com>
2023-03-22 14:14:49 +0100

    test

--- traitement.php
+++ traitement.php
@@ -1,4 +1,9 @@
 <?php
-$PALINDROME = $_GET['palindrome'];
-echo 'Bonjour '.$PALINDROME;
+$WORD = $_GET['word'];
+$INVERSE_WORD = strrev($WORD);
+if (strcmp($WORD,$INVERSE_WORD) == 1){
+    echo $WORD." est un palindrome"
+} else {
+    echo $WORD." n'est pas un palindrome"
+}
 ?>
